aaa56a8965332d2c805b0d3cb80f60341eb296cb
Jay209 <67544981+Jay209@users.noreply.github.com>
2022-11-26 21:26:36 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,47 @@
+from flask import Flask, render_template, request
+import os
+os.environ['CUDA_VISIBLE_DEVICES'] = '0'
+import cv2
+import numpy as np
+import tensorflow as tf
+from yolov3.utils import detect_image, detect_realtime, detect_video, Load_Yolo_model, detect_video_realtime_mp
+from yolov3.configs import *
+
+app = Flask(__name__)
+yolo = Load_Yolo_model()
+target_img = os.path.join(os.getcwd(), 'static/images')
+
+
+@app.route('/')
+def index_view():
+    return render_template('index.html')
+
+
+# Allow files with extension png, jpg and jpeg
+ALLOWED_EXT = set(['jpg', 'jpeg', 'png'])
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXT
+
+
+
+@app.route('/predict', methods=['GET', 'POST'])
+def predict():
+    if request.method == 'POST':
+        file = request.files['file']
+        if file and allowed_file(file.filename):
+            filename = file.filename
+            file_path = os.path.join('static/images', filename)
+            file_path_pred = os.path.join('static/predict', filename)
+            file.save(file_path)
+            # = read_image(file_path)
+            detect_image(yolo, file_path, file_path_pred, input_size=YOLO_INPUT_SIZE, show=False, CLASSES=TRAIN_CLASSES, rectangle_colors=(255,0,0))
+            return render_template('predict.html', fruit='horse', prob=0.9, user_image=file_path_pred)
+        else:
+            return "Unable to read the file. Please check file extension"
+
+
+if __name__ == '__main__':
+    app.run(debug=True, use_reloader=False, port=8000)
